fce60edd077d189a480dc3a32f0e691ef8dd4aa9
sakshi7171 <61040929+sakshi7171@users.noreply.github.com>
2020-07-16 13:23:36 +0530

    Add files via upload

---
+++ text.js
@@ -0,0 +1,42 @@
+console.log("hello")
+// Methods in string 
+let str = "The quick brown fox jumps over the wall";
+let str2 = "Hey guys how are you ";
+ console.log(str.length);
+console.log(str.toUpperCase);
+console.log(str.indexOf('e'));
+console.log(str.lastIndexOf('e'));
+console.log(str.link('fox'));
+console.log(str.toUpperCase());
+console.log(str.bold());
+console.log(str.charAt(5));
+console.log(str.concat(str2));
+console.log(str.fontcolor('blue'));
+console.log(str.fontsize(24));
+console.log(str.italics());
+console.log(str.toLowerCase());
+console.log(str.small());
+console.log(str.split());
+console.log(str.strike());
+console.log(str.toString());
+console.log(str.sup());
+console.log(str.substr(14));
+console.log(str.slice(4,10));
+
+
+
+//Methods in array 
+ 
+let arr = ['apple', 'ram', 'one','two','23','watermelon'];
+let arr2 = ['Priya','three','tree','5','10']
+console.log(arr);
+console.log(Array.isArray(arr));
+console.log(arr.concat(arr2));
+console.log(arr.indexOf('apple'));
+console.log(arr.length)
+console.log(arr.pop());
+console.log(arr.sort());
+console.log(arr.toString());
+console.log(arr.unshift('100'));
+console.log(arr);
+console.log(arr.values());
